b530443552c3e5dac38a3207ebb8e3df6eb19fa1
Drazen <pazinug@gmail.com>
2014-06-02 12:05:53 +0200

    Added proper seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Website.create([
-  { url: 'http://www.google.com/', category: 'test_category' },
-  { url: 'http://www.najdi.si/', category: 'test_category' },
-  { url: 'http://www.fri.uni-lj.si/', category: 'test_category' },
-  { url: 'http://www.stackoverflow.com/', category: 'test_category' },
-  { url: 'http://www.twitter.com/', category: 'test_category' },
-  { url: 'http://www.last.fm/', category: 'test_category' },
-  { url: 'http://www.coderwall.com/', category: 'test_category' },
-  { url: 'http://www.kinosiska.si/', category: 'test_category' },
-  { url: 'http://www.moonleerecords.com/', category: 'test_category' }
-               ])
+require 'csv'
+
+csv_text = File.read('db/seeds/websites_picked_200.csv')
+csv = CSV.parse(csv_text, :headers => false)
+csv.each do |row|
+  url = 'http://www.' + row[0]
+  Website.create!({ url: url })
+end
+
+#Website.create([
+#  { url: 'http://www.google.com/', category: 'test_category' },
+#  { url: 'http://www.najdi.si/', category: 'test_category' },
+#  { url: 'http://www.fri.uni-lj.si/', category: 'test_category' },
+#  { url: 'http://www.stackoverflow.com/', category: 'test_category' },
+#  { url: 'http://www.twitter.com/', category: 'test_category' },
+#  { url: 'http://www.last.fm/', category: 'test_category' },
+#  { url: 'http://www.coderwall.com/', category: 'test_category' },
+#  { url: 'http://www.kinosiska.si/', category: 'test_category' },
+#  { url: 'http://www.moonleerecords.com/', category: 'test_category' }
+#               ])
 
 # TODO: import websites through csv file
